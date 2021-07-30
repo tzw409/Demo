9cd4589b38573a4ddf28c4a948540a24da765c3e
tzw409 <tzw409@gmail.com>
2021-07-30 10:26:41 +0630

    add foldr

---
+++ app/app.py
@@ -0,0 +1,10 @@
+
+<h1> Hello Hi Vim</h1>
+<h1> Hello Hi Vim</h1>
+<h1> Hello Hi Vim</h1>
+<h1> Hello Hi Vim</h1>
+<h1> Hello Hi Vim</h1>
+<h1> Hello Hi Vim</h1>
+<h1> Hello Hi Vim</h1>
+<h1> Hello Hi Vim</h1>
+print("helo")
